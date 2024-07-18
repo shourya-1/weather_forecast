5fb0991ebd668703e9a9ae67678233355ec8a88f
shoraaz <shouryaaar@gmail.com>
2024-07-18 10:18:52 +0530

    Initial commit

---
+++ backend.py
@@ -0,0 +1,20 @@
+import os
+
+import requests
+
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
